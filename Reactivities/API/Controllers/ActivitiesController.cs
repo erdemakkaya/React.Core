fe4322d77a5d34152f6c9e3f77fce38c91ef59fd
erdemakkaya <50412064+erdemakkaya@users.noreply.github.com>
2020-02-14 22:58:19 +0300

    Activties ->Detail-And List added API-> Activity Controlelr Added

---
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Activities;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace DatingApp.API.Controllers {
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase {
+        private readonly IMediator _mediator;
+        public ActivitiesController (IMediator mediator) {
+        _mediator = mediator;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> Get () {
+            var values = await _mediator.Send (new List.Query());
+            return Ok (values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> Get(Guid id)
+        {
+            var value = await _mediator.Send(new Details.Query{Id=id });
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post ([FromBody] string value) { }
+
+        // PUT api/values/5
+        [HttpPut ("{id}")]
+        public void Put (int id, [FromBody] string value) { }
+
+        // DELETE api/values/5
+        [HttpDelete ("{id}")]
+        public void Delete (int id) { }
+    }
+}
