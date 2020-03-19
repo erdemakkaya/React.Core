c37a8acf442567c16b11206ff7fbd530190aaf8a
erdemakkaya <50412064+erdemakkaya@users.noreply.github.com>
2020-03-19 21:56:40 +0300

    last update
    last commit

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<IActivity|null>(null
   );
-  const handleSelectActivity = (id: string) => {
+  const handleSelectActivity = (id:string ) => {
     setSelectedActivity(activities.filter(a => a.id === id)[0]);
     setEditMode(false);
   };
