7e8c243b2a9805153d9ae86eaeac15b0f0d4ca6e
erdemakkaya <50412064+erdemakkaya@users.noreply.github.com>
2020-03-31 21:52:09 +0300

    Server Side Exception Handling

--- Reactivities/Application/Activities/Details.cs
+++ Reactivities/Application/Activities/Details.cs
@@ -1,8 +1,10 @@
-﻿using Domain;
+﻿using Application.Activities.Errors;
+using Domain;
 using MediatR;
 using Persistence;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -25,7 +27,10 @@ namespace Application.Activities
             }
             public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
             {
+                 
                 var activity = await _context.Activities.FindAsync(request.Id);
+                 if (activity == null)
+                    throw new RestException(HttpStatusCode.NotFound,new{activity="Not found"});
                 return activity;
             }
         }
